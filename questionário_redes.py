1f7dd0c538159a178a575f550001123e31e85e8b
Cristiano <alunocristianohm@gmail.com>
2024-09-18 14:27:39 -0300

    first commit

---
+++ questionário_redes.py
@@ -0,0 +1,77 @@
+print ('''Bem vindos ao questionário sobre redes, escolha a alternativa e veja se está correta''')
+
+print('''Vamos começar
+      ''')
+
+print ('1- Qual das seguintes afirmações sobre a máscara de sub-rede é verdadeira?')
+print ('A) A máscara de sub-rede é um número de 64 bits usado para criptografar endereços IP.')
+print ('B) A máscara de sub-rede é utilizada para separar um endereço IP em partes de rede e host.')
+print ('C) A máscara de sub-rede é uma configuração opcional em redes IP e não afeta a comunicação entre dispositivos.')
+
+resposta = input ('digite uma das opções?')
+
+if resposta == 'b':
+    print ('resposta certa')
+if resposta == 'a':
+    print ('resposta errada')
+if resposta == 'c':
+    print ('resposta errada')
+
+print ('2- Qual é a função dos conectores RJ-45 em um cabo de rede Ethernet?')
+print ('A) Transmitir sinais de áudio e vídeo para dispositivos de entretenimento.')
+print ('B) Interligar dispositivos por meio de cabos de par trançado, permitindo a comunicação de dados.')
+print ('C) Conectar dispositivos a uma fonte de alimentação elétrica.')
+
+resposta = input ('digite uma das opções?')
+
+if resposta == 'b':
+    print ('resposta certa')
+if resposta == 'a':
+    print ('resposta errada')
+if resposta == 'c':
+    print ('resposta errada')
+
+print ('3- Qual dos seguintes padrões de cabeamento Ethernet é mais comum em ambientes corporativos e domésticos?')
+print ('A) T568A')
+print ('B) T568B')
+print ('C) T568C')
+
+
+resposta = input ('digite uma das opções?')
+
+if resposta == 'b':
+    print ('resposta certa')
+if resposta == 'a':
+    print ('resposta errada')
+if resposta == 'c':
+    print ('resposta errada')
+
+print ('4- Qual das seguintes especificações Ethernet é conhecida como Gigabit Ethernet?')
+print ('A) Ethernet 10 MB/s')
+print ('B) Ethernet 100 MB/s')
+print ('C) Ethernet 1000 MB/s')
+
+
+resposta = input ('digite uma das opções?')
+
+if resposta == 'c':
+    print ('resposta certa')
+if resposta == 'a':
+    print ('resposta errada')
+if resposta == 'b':
+    print ('resposta errada') 
+
+print ('5- Qual das seguintes categorias de cabo de rede suporta velocidades de até 10 Gbps em distâncias de até 100 metros?')
+print ('A) CAT 5')
+print ('B) CAT 6')
+print ('C) CAT 7')
+
+
+resposta = input ('digite uma das opções?')
+
+if resposta == 'c':
+    print ('resposta certa')
+if resposta == 'a':
+    print ('resposta errada')
+if resposta == 'b':
+    print ('resposta errada')
